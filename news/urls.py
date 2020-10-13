6d21c98406cc6b780baaa85741367e1598b36786
陆艇 <luting@jane7.com>
2020-10-13 22:59:40 +0800

    [功能新增](V0.2.2)新闻分类&新闻管理分页

--- news/urls.py
+++ news/urls.py
@@ -7,8 +7,8 @@ app_name = 'news'
 urlpatterns = [
     path('main/', views.main, name='main'),
     path('welcome/', views.welcome, name='welcome'),
-    path('category/', views.category_list, name='category'),
+    path('category_list/', views.CategoryListView.as_view(), name='category_list'),
     path('add_category/', views.add_category, name='add_category'),
-    path('new/', views.new_list, name='new'),
+    path('new_list/', views.NewListView.as_view(), name='new_list'),
     path('add_new/', views.add_news, name='add_new')
 ]
--- news/views.py
+++ news/views.py
@@ -1,21 +1,14 @@
+import os
+# import pandas as pd
 from django.http import JsonResponse
 from django.shortcuts import render
-
-
-# Create your views here.
-from news.models import NewsCategory
+from django.views.generic import ListView
+from celery_task.settings import BASE_DIR
+from news.models import NewsCategory, News
 
 
 def main(request):
     return render(request, 'index.html')
-
-
-def category_list(request):
-    return render(request, 'news_category_list.html')
-
-
-def new_list(request):
-    return render(request, 'news_new_list.html')
 
 
 def welcome(request):
@@ -23,7 +16,6 @@ def welcome(request):
 
 
 def add_category(request):
-
     if request.method == 'GET':
         return render(request, 'news_category_add.html')
 
@@ -36,9 +28,112 @@ def add_category(request):
 
 
 def add_news(request):
-
     if request.method == 'GET':
         return render(request, 'news_new_add.html')
 
     if request.method == 'POST':
+        file = request.FILES.get('exc_file')
+        stream = open(os.path.join(BASE_DIR, 'medias', 'excel_files', file.name), 'wb')
+        for chunk in file.chunks():
+            stream.write(chunk)
+        stream.close()
+        # df = pd.read_excel(f"{BASE_DIR}/medias/excel_files/{file.name}")
         return JsonResponse({"code": 200, "msg": "导入数据成功"})
+
+
+class CategoryListView(ListView):
+    model = NewsCategory
+    template_name = 'news_category_list.html'
+    context_object_name = 'newsCategorys'
+    paginate_by = 10
+    ordering = 'c_time'
+    page_kwarg = 'p'
+
+    def get_context_data(self, **kwargs):
+        context = super(CategoryListView, self).get_context_data(*kwargs)
+        paginator = context.get('paginator')
+        page_obj = context.get('page_obj')
+        pagination_data = self.get_pagination_data(paginator, page_obj, 3)
+        context.update(pagination_data)
+        return context
+
+    def get_pagination_data(self, paginator, page_obj, around_count=2):
+        current_page = page_obj.number
+        num_pages = paginator.num_pages
+
+        left_has_more = False
+        right_has_more = False
+
+        if current_page <= around_count + 2:
+            left_pages = range(1, current_page)
+        else:
+            left_has_more = True
+            left_pages = range(current_page - around_count, current_page)
+
+        if current_page >= num_pages - around_count - 1:
+            right_pages = range(current_page + 1, num_pages + 1)
+        else:
+            right_has_more = True
+            right_pages = range(current_page + 1, current_page + around_count + 1)
+
+        # 查询总条数
+        count = NewsCategory.objects.all().count()
+
+        return {
+            'left_pages': left_pages,
+            'right_pages': right_pages,
+            'current_page': current_page,
+            'left_has_more': left_has_more,
+            'right_has_more': right_has_more,
+            'num_pages': num_pages,
+            'count': count,
+        }
+
+
+class NewListView(ListView):
+    model = News
+    template_name = 'news_new_list.html'
+    context_object_name = 'news'
+    paginate_by = 10
+    ordering = 'id'
+    page_kwarg = 'p'
+
+    def get_context_data(self, **kwargs):
+        context = super(NewListView, self).get_context_data(*kwargs)
+        paginator = context.get('paginator')
+        page_obj = context.get('page_obj')
+        pagination_data = self.get_pagination_data(paginator, page_obj, 3)
+        context.update(pagination_data)
+        return context
+
+    def get_pagination_data(self, paginator, page_obj, around_count=2):
+        current_page = page_obj.number
+        num_pages = paginator.num_pages
+
+        left_has_more = False
+        right_has_more = False
+
+        if current_page <= around_count + 2:
+            left_pages = range(1, current_page)
+        else:
+            left_has_more = True
+            left_pages = range(current_page - around_count, current_page)
+
+        if current_page >= num_pages - around_count - 1:
+            right_pages = range(current_page + 1, num_pages + 1)
+        else:
+            right_has_more = True
+            right_pages = range(current_page + 1, current_page + around_count + 1)
+
+        # 查询总条数
+        count = News.objects.all().count()
+
+        return {
+            'left_pages': left_pages,
+            'right_pages': right_pages,
+            'current_page': current_page,
+            'left_has_more': left_has_more,
+            'right_has_more': right_has_more,
+            'num_pages': num_pages,
+            'count': count,
+        }
